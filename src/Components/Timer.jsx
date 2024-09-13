373d8b7e910b6aeefed9b17f5b8a52c157478c42
DanishAliUmar <danishaliumar4@gmail.com>
2024-09-14 00:00:51 +0500

    Fix some issues ...

--- src/Components/Timer.jsx
+++ src/Components/Timer.jsx
@@ -126,7 +126,7 @@ const Timer = ({ onTimeUpdate, staticMinutes = null, staticSeconds = null }) => 
             );
         }
         return items;
-    };`
+    };
 
     return (
         <div className='bg-slate-100 flex items-center justify-center w-screen h-screen'>
